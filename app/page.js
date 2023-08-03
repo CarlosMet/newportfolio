0cab2d116661cbe3f55c38bad0939dd554055dce
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-03 02:29:42 -0500

    1st commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Hero from "@/Components/Hero";
+import Navbar from "@/Components/Navbar";
+import Topbar from "@/Components/Topbar";
+
+
+export default function Home() {
+  return (
+    <div className="relative">
+      <Topbar></Topbar>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </div>
+  )
+}
